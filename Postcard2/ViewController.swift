6f414cc03d32b99042a2d0b939bcc23262a95003
David Schneider <davidschneider@davids-mac-pro.attlocal.net>
2015-06-15 16:33:42 -0700

    Added Name label stuff

--- Postcard2/ViewController.swift
+++ Postcard2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +37,16 @@ class ViewController: UIViewController {
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
-        mailButton.setTitle("Fuck you.", forState: UIControlState.Normal)
+        mailButton.setTitle("Sent", forState: UIControlState.Normal)
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+    
+    
+        
 }
 
     
